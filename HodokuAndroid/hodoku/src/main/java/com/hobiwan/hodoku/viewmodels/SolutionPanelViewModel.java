cfa62fcd9734e7ea22a9d741955f57d553796f62
Justin Nordin <jlnordin@gmail.com>
2020-02-14 15:21:50 -0800

    Fixup view model API requirements for GuiState.java.

--- HodokuAndroid/hodoku/src/main/java/com/hobiwan/hodoku/viewmodels/SolutionPanelViewModel.java
+++ HodokuAndroid/hodoku/src/main/java/com/hobiwan/hodoku/viewmodels/SolutionPanelViewModel.java
@@ -1,4 +1,12 @@
 package com.hobiwan.hodoku.viewmodels;
 
+import com.hobiwan.hodoku.sudoku.GuiState;
+
 public class SolutionPanelViewModel {
+
+    public void getState(GuiState state, boolean copy) {
+    }
+
+    public void setState(GuiState state) {
+    }
 }
